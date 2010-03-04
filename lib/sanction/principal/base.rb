e4e22f07baefd4c95296aad2969726bc60360b72
Alex Neth <alex@liivid.com>
2010-03-04 04:32:23 -0800

    When generating this join, rails seems to munge it, but reversing the join order fixes it.  This is the SQL (Postgres) generated otherwise:
    INNER JOIN sanction_roles AS sanction_permissionable_role_query ON
    'Team' AND = sanction_permissionable_role_query.permissionable_type

--- lib/sanction/principal/base.rb
+++ lib/sanction/principal/base.rb
@@ -14,8 +14,9 @@ module Sanction
  
           returned_scope = {:conditions => ["#{ROLE_ALIAS}.principal_type = ?", base.name.to_s], :select => "DISTINCT #{base.table_name}.*"}
           unless already_joined
-            returned_scope.merge( {:joins => "INNER JOIN #{Sanction::Role.table_name} AS #{ROLE_ALIAS} ON #{ROLE_ALIAS}.principal_type = '#{base.name.to_s}' AND 
-              (#{ROLE_ALIAS}.principal_id = #{base.table_name}.#{base.primary_key.to_s} OR #{ROLE_ALIAS}.principal_id IS NULL)"} )
+            returned_scope.merge( {:joins => "INNER JOIN #{Sanction::Role.table_name} AS #{ROLE_ALIAS} ON 
+              (#{ROLE_ALIAS}.principal_id = #{base.table_name}.#{base.primary_key.to_s} OR #{ROLE_ALIAS}.principal_id IS NULL)
+              AND #{ROLE_ALIAS}.principal_type = '#{base.name.to_s}'"} )
           end
         }
 
@@ -24,8 +25,9 @@ module Sanction
          
           returned_scope = {:conditions => ["#{klass_instance.class.table_name}.#{klass_instance.class.primary_key.to_s} = ?", klass_instance.id], :select => "DISTINCT #{klass_instance.class.table_name.to_s}.*"}
           unless already_joined
-            returned_scope.merge({:joins => "INNER JOIN #{Sanction::Role.table_name} AS #{ROLE_ALIAS} ON #{ROLE_ALIAS}.principal_type = '#{klass_instance.class.name.to_s}' AND
-              (#{ROLE_ALIAS}.principal_id = '#{klass_instance.id}' OR #{ROLE_ALIAS}.principal_id IS NULL)"})
+            returned_scope.merge({:joins => "INNER JOIN #{Sanction::Role.table_name} AS #{ROLE_ALIAS} ON
+              (#{ROLE_ALIAS}.principal_id = '#{klass_instance.id}' OR #{ROLE_ALIAS}.principal_id IS NULL) AND
+              #{ROLE_ALIAS}.principal_type = '#{klass_instance.class.name.to_s}'"})
           end
         }
       end
