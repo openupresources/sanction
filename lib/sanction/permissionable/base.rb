81eaeccc72152355ffc0d23c0295c7c6f97b6e28
Alex Neth <alex@liivid.com>
2010-03-04 03:43:37 -0800

    Missed some references to roles table. To configure the table name:     Sanction::Role.set_table_name "sanction_roles"

--- lib/sanction/permissionable/base.rb
+++ lib/sanction/permissionable/base.rb
@@ -4,7 +4,7 @@ module Sanction
       def self.extended(base)
         base.class_eval %q{
           has_many :permissionable_roles, :as => :permissionable, :class_name => "Sanction::Role",
-                   :finder_sql => 'SELECT * FROM roles WHERE roles.permissionable_type = "#{self.class.name.to_s}" AND (roles.permissionable_id = "#{id}" OR roles.permissionable_id IS NULL)'
+                   :finder_sql => 'SELECT * FROM #{Sanction::Role.table_name} WHERE #{Sanction::Role.table_name}.permissionable_type = "#{self.class.name.to_s}" AND (#{Sanction::Role.table_name}.permissionable_id = "#{id}" OR #{Sanction::Role.table_name}.permissionable_id IS NULL)'
  
           has_many :specific_permissionable_roles, :as => :permissionable, :class_name => "Sanction::Role", :dependent => :destroy
         }
@@ -14,7 +14,7 @@ module Sanction
 
           returned_scope = {:conditions => ["#{ROLE_ALIAS}.permissionable_type = ?", base.name.to_s], :select => "DISTINCT #{base.table_name}.*"}
           unless already_joined
-            returned_scope.merge({:joins => "INNER JOIN roles AS #{ROLE_ALIAS} ON #{ROLE_ALIAS}.permissionable_type = '#{base.name.to_s}' AND
+            returned_scope.merge({:joins => "INNER JOIN #{Sanction::Role.table_name} AS #{ROLE_ALIAS} ON #{ROLE_ALIAS}.permissionable_type = '#{base.name.to_s}' AND
               (#{ROLE_ALIAS}.permissionable_id = #{base.table_name}.#{base.primary_key.to_s} OR #{ROLE_ALIAS}.permissionable_id IS NULL)"})
           end
         }
@@ -24,7 +24,7 @@ module Sanction
    
           returned_scope = {:conditions => ["#{klass_instance.class.table_name}.#{klass_instance.class.primary_key.to_s} = ?", klass_instance.id], :select => "DISTINCT #{klass_instance.class.table_name}.*"}
           unless already_joined
-            returned_scope.merge({:joins => "INNER JOIN roles AS #{ROLE_ALIAS} ON #{ROLE_ALIAS}.permissionable_type = '#{klass_instance.class.name.to_s}' AND 
+            returned_scope.merge({:joins => "INNER JOIN #{Sanction::Role.table_name} AS #{ROLE_ALIAS} ON #{ROLE_ALIAS}.permissionable_type = '#{klass_instance.class.name.to_s}' AND 
               (#{ROLE_ALIAS}.permissionable_id = '#{klass_instance.id}' OR #{ROLE_ALIAS}.permissionable_id IS NULL)"})
           end
         }
